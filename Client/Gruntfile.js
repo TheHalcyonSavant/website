0655a994d23a21a8f381223fec9be59881d5c681
TheHalcyonSavant <TheHalcyonSavant@gmail.com>
2014-09-22 12:34:02 +0200

    autocopy CV

--- Client/Gruntfile.js
+++ Client/Gruntfile.js
@@ -396,7 +396,7 @@ module.exports = function (grunt) {
           cwd: '<%= yeoman.app %>',
           dest: '<%= yeoman.dist %>',
           src: [
-            '*.{ico,png,txt}',
+            '*.{ico,png,txt,pdf}',
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
